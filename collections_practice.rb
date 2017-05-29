283aa29fa907b162537437c87293b2753dc41f1d
Tony Nassif <tonynassif@gmail.com>
2017-05-29 20:41:38 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,3 +1,11 @@
 def sort_array_asc(array)
   array.sort
 end
+
+def sort_array_desc(array)
+  array.sort { |x, y| y <=> x}
+end
+
+def sort_array_char_count(array)
+  array.sort { |x, y| x.length <=> y.length }
+end
