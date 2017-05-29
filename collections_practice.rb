60d852eed9b30e79d7a40619a3d39f01c376ab65
Tony Nassif <tonynassif@gmail.com>
2017-05-29 22:48:09 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -15,6 +15,11 @@ def swap_elements(array)
   return array
 end
 
+def swap_elements_from_to(array, index, destination_index)
+  array[index], array[destination_index] = array[destination_index], array[index]
+  return array
+end
+
 def reverse_array(array)
   array.reverse
 end
